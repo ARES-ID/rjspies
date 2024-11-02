8c8b8c1c3e64aa14e86576d06fd2ae784e608547
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2024-11-02 23:57:54 +0100

    Add detekt and fix libs (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
-    alias(libs.plugins.composeMultiplatform) apply false
-    alias(libs.plugins.composeCompiler) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.orgJetbrainsCompose) apply false
+    alias(libs.plugins.orgJetbrainsKotlinPluginCompose) apply false
+    alias(libs.plugins.orgJetbrainsKotlinMultiplatform) apply false
+    alias(libs.plugins.ioGitlabArturboschDetekt) apply false
 }
